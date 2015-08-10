46d41165d2eab80957556f30aa266e2cc25ae126
EmanuelaLetia <emanuela.letia@stud.uni-regensburg.de>
2015-08-10 12:28:05 +0200

    Initial commit

---
+++ app/src/androidTest/java/lee51877/iw/ur/de/gardenproject/ApplicationTest.java
@@ -0,0 +1,13 @@
+package lee51877.iw.ur.de.gardenproject;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
