b5843abcd8ab95448aa05e3a43ecc41c7e604061
EmanuelaLetia <emanuela.letia@stud.uni-regensburg.de>
2015-08-10 13:04:34 +0200

    GardenAPI class + info

--- app/src/main/java/lee51877/iw/ur/de/gardenproject/util/GardenAPI.java
+++ app/src/main/java/lee51877/iw/ur/de/gardenproject/util/GardenAPI.java
@@ -1,7 +1,28 @@
 package lee51877.iw.ur.de.gardenproject.util;
 
+import org.scribe.builder.api.DefaultApi10a;
+import org.scribe.model.Token;
+
 /**
  * Created by Ema on 10.08.2015.
  */
-public class GardenAPI {
+public class GardenAPI  extends DefaultApi10a {
+    @Override
+    public String getAccessTokenEndpoint()
+    {
+        return GardenConstants.ACCESS_TOKEN;
+    }
+
+    @Override
+    public String getRequestTokenEndpoint()
+    {
+        return GardenConstants.REQUEST_TOKEN;
+    }
+
+    @Override
+    public String getAuthorizationUrl(Token requestToken)
+    {
+        return String.format(GardenConstants.AUTHORIZE_URL, requestToken.getToken());
+    }
+
 }
